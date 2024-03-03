56e56e9e819aad36cfc560b29250ac53e55b31b1
Adilan <adilan.akhramovich@gmail.com>
2024-03-03 16:17:09 +0500

    added a new functiopn to translate and a fonts Eastman Bold

--- lang.js
+++ lang.js
@@ -1,7 +1,79 @@
 const langArr = {
-    "cta": {
+    "headTitle": {
+        "ru": "Алюминий Atmosfar",
+        "en": "Atmosfar Aluminium"
+    },
+    "navHome": {
+        "ru": "ГЛАВНАЯ",
+        "en": "HOME"
+    },
+    "navAbout": {
+        "ru": "О НАС",
+        "en": "ABOUT"
+    },
+    "navProducts": {
+        "ru": "ПРОДУКЦИЯ",
+        "en": "PRODUCTS"
+    },
+    "navDownloads": {
+        "ru": "СЕРТЕФИКАТЫ И ЗАГРУЗКИ",
+        "en": "CERTEFICATES AND DOWNLOADS"
+    },
+    "navContact": {
+        "ru": "СВЯЖИТЕСЬ С НАМИ",
+        "en": "CONTACT US"
+    },
+    "headerDescription": {
+        "ru": "Это неоспоримая красота классического дизайна каркасных конструкций, увеличенные требования к энергоэффективности, разнообразие элементов и типов открывания, множество вариантов цветов, настоящее шведское качество, умноженное на большое количество тестирований.",
+        "en": "This is the undeniable beauty of the classic design of frame structures, increased requirements for energy efficiency, a variety of elements and types of opening, many color options, real Swedish quality multiplied by a large amount of testing."
+    },
+    "headerDescription2": {
+        "ru": "Посетите нас, чтобы узнать больше",
+        "en": "Visit Us To know More"
+    },
+    "offerHeading": {
+        "ru": "Что мы предлагаем",
+        "en": "What we offer"
+    },
+    "offerText1": {
+        "ru": "1. Atmosfar производит большое количество архитектурных алюминиевых профилей, также известных как алюминиевые строительные профили, используемые для строительства и строительных областей, таких как окна и двери, купольные стены, ограждения и поручни, жалюзи и навесы. Эти строительные алюминиевые профили включают в себя как стандартные, так и системы с терморазрывом.",
+        "en": "1. Atmosfar produces a large number of architectural aluminium profiles also known as aluminum construction profiles, used for building and construction areas such as window and doors, curtain walls, fencing and handrail, louvre and walkway covers. These construction aluminium profiles include both standard and thermal-break systems."
+    },
+    "offerText2": {
+        "ru": "2. Наши архитектурные алюминиевые экструзии для архитектурных систем доступны в различных отделках, которые включают в себя мельничную отделку, прозрачную анодировку, электролитическую анодировку, порошковую покраску, покраску PVDF и отделку под дерево. Мы также предлагаем услуги изготовления на заказ архитектурных алюминиевых экструзионных профилей на основе ваших потребностей.",
+        "en": "2. Our architectural aluminum extrusions for architectural systems are available in a variety of finishes which contain mill finish, clear anodized, electrolytic anodizing, powder-coating, PVDF painting and woodgrain finish. We also offer fabrication services of customized architectural aluminum extrusion profiles based on your needs."
+    },
+    "offerText3": {
+        "ru": "3. Все запросы делаются на заказ. При заказе алюминиевых экструзионных строительных профилей, пожалуйста, укажите свои требования по сплаву, состоянию, длине, количеству, отделке поверхности, конечному использованию и любые специальные инструкции относительно допуска, доставки, упаковки и сертификации. Было бы здорово, если бы вы смогли поделиться с нами вашими разработанными чертежами.",
+        "en": "3. All requests are made to orders. When ordering aluminum extrusions construction profiles, please specify your requirements about alloy, temper, length, quantity, surface finish, end-use and any special instructions regarding tolerance, delivery, packing and certification. It couldn't be better if you can share with us your designed drawings."
+    },
+    "buildingHeading": {
+        "ru": "Некоторые здания, выполненные из алюминия",
+        "en": "Some buildings that was made of Aluminium"
+    },
+    "buildingText": {
+        "ru": "Множество новшеств в производстве в современном мире требует новых производственных идей и компонентов с большим количеством преимуществ по сравнению с существующими, в каждом аспекте, включая физические и химические свойства. В области производства процесс экструзии имеет широкий спектр применения.",
+        "en": "Many advancements in manufacturing in this modern world requesting new manufacturing ideas and components with more advantages than the existing one in every aspect including physical and chemical properties. In manufacturing field, extrusion process has a wide range of application."
+    },
+    "facilitiesHeading": {
+        "ru": "Наши продукты",
+        "en": "Our Products"
+    },
+    "facilitiesText1": {
+        "ru": "История о Алюминии, и некоторые факты.",
+        "en": "History about Aluminium, and some facts."
+    },
+    "facilitiesText2": {
+        "ru": "Алюминий - это химический элемент. Это мягкий, немагнитный и дуктильный материал в борной группе. Главной рудой алюминия является боксит. Алюминий - серебристо-белый металл с 13-м элементом в периодической таблице. Его атомный номер равен 13, а его атомная масса составляет 26,98153, его также часто называют Al.",
+        "en": "Aluminum is a chemical element. It is a soft, non-magnetic and ductile material in boron group. The chief ore of aluminum is Bauxite. Aluminum is a silvery white metal with a 13th element in the periodic table. Its atomic number is 13 and 26.98153 is its atomic mass and it is popularly known as Al."
+    },
+    "facilitiesText3": {
+        "ru": "Сплав обычно называется смесью двух или более металлов, полученной путем плавления и смешивания выбранных металлов. Таким образом, смесь, полученная при смешивании, имеет некоторые различия и преимущества в свойствах по сравнению с отдельными металлами. Алюминиевые сплавы, в которых алюминий (Al) используется как основной металл. Медь, магний, марганец, кремний, олово и цинк являются наиболее распространенными типичными элементами легирования.",
+        "en": "An alloy is commonly known as the mixture of two or more metals made by melting and mixing the selected metals. Thus a mixture obtained from mixing has some different and advancement in properties from individual metals. Aluminum alloys are which aluminum (Al) is employed as a major metal. Copper, magnesium, manganese, silicon, tin and zinc are the most commonly using typical alloying elements."
+    },
+    "ctaText": {
         "ru": "Позвоните нам<br>или свяжитесь с нами по электронной почте",
-        "en": "CALL US<br>OR CONTACT US VIA EMAIL",
-        "ua": "Телефонуйте нам<br>або зв'яжіться з нами за електронною поштою"
-    },    
-}
+        "en": "CALL US<br>OR CONTACT US VIA EMAIL"
+    },
+};
+
